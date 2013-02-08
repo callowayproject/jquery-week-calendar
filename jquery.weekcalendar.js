91da3c3cf40d26232e15922b449c02b8a5379d81
unknown <jnicoll2@IS-APPS-0133.is.ed.ac.uk>
2013-02-08 17:02:18 +0000

    Replaced use of jQuery.browser with feature detection, for compatibility with jQuery 1.9 and above

--- jquery.weekcalendar.js
+++ jquery.weekcalendar.js
@@ -2051,9 +2051,9 @@
        */
       _disableTextSelect: function($elements) {
           $elements.each(function() {
-            if ($.browser.mozilla) {//Firefox
+            if (typeof this.style.MozUserSelect !== 'undefined') {//Firefox
                 $(this).css('MozUserSelect', 'none');
-            } else if ($.browser.msie) {//IE
+            } else if (typeof this.onselectstart !== 'undefined') {//IE
                 $(this).bind('selectstart', function() {
                   return false;
                 });
